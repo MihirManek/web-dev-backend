932db2bf4a067c541ca6d484017ee7932da8fa1b
Mihir Manek <mihir.manek@somaiya.edu>
2022-04-04 18:59:34 -0400

    initial commit

---
+++ controllers/tuits.js
@@ -0,0 +1,125 @@
+export default [
+  {
+    _id: "123",
+    topic: "Web Development",
+    postedBy: {
+      username: "ReactJS",
+    },
+    liked: true,
+    verified: false,
+    handle: "ReactJS",
+    time: "2h",
+    title:
+      "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+    tuit: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    attachments: {
+      video: "unKvMC3Y1kI",
+    },
+    logoImage: "../../../images/react-blue.png",
+    avatarImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRye0H0xrFuy5NL8B7hzObWUh7ERoU4z-jyOg&usqp=CAU",
+    stats: {
+      comments: 123,
+      retuits: 234,
+      likes: 345,
+      dislikes: 345,
+    },
+  },
+  {
+    _id: "567",
+    topic: "Space",
+    postedBy: {
+      username: "SpaceX",
+    },
+    liked: false,
+    verified: false,
+    handle: "spacex",
+    title: "",
+    tuit: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    attachments: {
+      image:
+        "https://techcrunch.com/wp-content/uploads/2019/09/Starship-Mk1-Day.jpg",
+    },
+    time: "last month",
+    logoImage: "../../../images/node.png",
+    avatarImage: "https://www.spacex.com/static/images/share.jpg",
+    tuits: "120K",
+    stats: {
+      comments: 123,
+      retuits: 234,
+      likes: 345,
+      dislikes: 345,
+    },
+  },
+  {
+    _id: "234",
+    topic: "Space",
+    postedBy: {
+      username: "Scott Manley",
+    },
+    liked: true,
+    verified: true,
+    handle: "DJSnM",
+    postedOn: "2020-12-10",
+    time: "1 day",
+    title:
+      "JavaScript is programming language that can run on browsers as well as desktops",
+    tuit: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    logoImage: "../../../images/js.png",
+    avatarImage:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Scott_Manley_face.png/220px-Scott_Manley_face.png",
+    tuits: "123K",
+    stats: {
+      comments: 123,
+      retuits: 234,
+      likes: 345,
+      dislikes: 345,
+    },
+  },
+  {
+    _id: "345",
+    topic: "Web Development",
+    postedBy: {
+      username: "jQuery",
+    },
+    liked: false,
+    verified: false,
+    handle: "jQuery",
+    title: "jQuery",
+    tuit: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    time: "last week",
+    logoImage: "../../../images/jquery.png",
+    avatarImage:
+      "https://openjsf.org/wp-content/uploads/sites/84/2019/10/jquery-logo-vertical_large_square.png",
+    tuits: "122K",
+    stats: {
+      comments: 123,
+      retuits: 234,
+      likes: 345,
+      dislikes: 345,
+    },
+  },
+  {
+    _id: "456",
+    topic: "Web Development",
+    postedBy: {
+      username: "NodeJS",
+    },
+    liked: true,
+    verified: true,
+    handle: "NodeJS",
+    title: "",
+    tuit: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    time: "last month",
+    logoImage: "../../../images/node.png",
+    avatarImage:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfOfptrC48Ggz3JC23XVvfUc6qPqMrkrGGZ5ajLqLDARAP15-8lnl4ETCV_BNdMoW7TjU&usqp=CAU",
+    tuits: "120K",
+    stats: {
+      comments: 123,
+      retuits: 234,
+      likes: 345,
+      dislikes: 345,
+    },
+  },
+];
